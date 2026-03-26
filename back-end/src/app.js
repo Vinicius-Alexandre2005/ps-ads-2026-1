4cde38123bf67e4d57e28f1c9435a8f52abe2620
Vinícius <vinimendes291@hotmail.com>
2026-03-26 15:17:44 +0000

    (26/03) Início da integração com front-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,9 +3,15 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
 
 const app = express()
+
+import cors from 'cors'
+
+app.use(cors({
+ origin: process.env.ALLOWED_ORIGINS.split(','),
+ // credentials: true   // Habilita o envio de cookies para o front-end
+}))
 
 app.use(logger('dev'))
 app.use(json())
@@ -13,11 +19,16 @@ app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 /**************** ROTAS *******************/
 
 import customersRoute from './routes/customers.js'
 app.use('/customers', customersRoute)
+
+import carsRoute from './routes/cars.js'
+app.use('/cars', carsRoute)
+
+import usersRoute from './routes/users.js'
+app.use('/cars', usersRoute)
 
 export default app
